28a90290b83c5b8db1e46c5dc7045349845b230f
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-11-04 13:03:48 -0300

    Primeiro commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+app.get
+
+app.listen(3001, () => {
+    console.log("Servidor rodando na porta: 3001");
+});
