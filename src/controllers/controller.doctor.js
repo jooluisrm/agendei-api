d0784f3b18b4c5281e35e70eea8ee4dc7f0d949e
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-11-04 15:47:25 -0300

    service criado

--- src/controllers/controller.doctor.js
+++ src/controllers/controller.doctor.js
@@ -1,5 +1,10 @@
-function Listar(req, res) {
-    res.status(200).send("Aqui vamos listar os medicos");
+import serviceDoctor from "../services/service.doctor.js";
+
+async function Listar(req, res) {
+
+    const doctors = await serviceDoctor.Listar();
+
+    res.status(200).json(doctors);
 }
 
 export default { Listar };
