0906fd08bd76258192b363dbbfee6e682b8a7d4c
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-11-05 09:01:57 -0300

    banco de dados funcionando

--- src/repositories/repository.doctor.js
+++ src/repositories/repository.doctor.js
@@ -1,12 +1,11 @@
+import { query } from "../database/sqlite.js";
+
 async function Listar() {
 
-    // Simula o acesso ao banco
-    const doctors = [
-        {id: 1, name: "Heber", specialty: "Cardiologista", icon: "M"},
-        {id: 2, name: "João", specialty: "Clínico Geral", icon: "M"},
-        {id: 3, name: "Maria", specialty: "Cardiologista", icon: "F"}
-    ];
+    let sql = "select * from doctors order by name";
 
+    const doctors = await query(sql, []);
+    
     return doctors;
 };
 
