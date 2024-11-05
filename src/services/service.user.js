10d4cda59eab2b8604d894801e69ac2978cb6dac
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-11-05 12:20:46 -0300

    token de acesso feito

--- src/services/service.user.js
+++ src/services/service.user.js
@@ -1,11 +1,14 @@
 import repositoryUser from "../repositories/repository.user.js";
 import bcrypt from "bcrypt";
+import jwt from "../token.js";
 
 async function Inserir(name, email, password) {
 
     const hashPassword = await bcrypt.hash(password, 10);
-
     const user = await repositoryUser.Inserir(name, email, hashPassword);
+
+    user.token = jwt.CreateToken(user.id_user);
+
     return user;
 };
 
@@ -19,6 +22,9 @@ async function Login(email, password) {
     } else {
         if (await bcrypt.compare(password, user.password)) {
             delete user.password;
+
+            user.token = jwt.CreateToken(user.id_user);
+
             return user;
         } else {
             return [];
