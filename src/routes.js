f3e56cef88c0a39ac53c5438e9532fd8c8a5fdd0
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-11-13 10:41:27 -0300

    rotas do admin criado

--- src/routes.js
+++ src/routes.js
@@ -19,6 +19,9 @@ router.post("/users/register", controllerUser.Inserir);
 router.post("/users/login", controllerUser.Login);
 router.get("/users/profile", jwt.ValidateToken, controllerUser.Profile);
 
+router.post("/admin/register", controllerUser.InserirAdmin);
+router.post("/admin/login", controllerUser.LoginAdmin);
+
 // Reservas
 router.get("/appointments", jwt.ValidateToken, controllerAppointment.ListarByUser);
 router.post("/appointments", jwt.ValidateToken, controllerAppointment.Inserir);
--- src/services/service.user.js
+++ src/services/service.user.js
@@ -38,4 +38,36 @@ async function Profile(id_user) {
     return user;
 };
 
-export default { Inserir, Login, Profile };
+//Login & register ADMIN
+
+async function InserirAdmin(name, email, password) {
+
+    const hashPassword = await bcrypt.hash(password, 10);
+    const user = await repositoryUser.InserirAdmin(name, email, hashPassword);
+
+    user.token = jwt.CreateToken(user.id_user);
+
+    return user;
+};
+
+async function LoginAdmin(email, password) {
+
+
+    const user = await repositoryUser.ListarByEmailAdmin(email);
+
+    if (user.length === 0) {
+        return [];
+    } else {
+        if (await bcrypt.compare(password, user.password)) {
+            delete user.password;
+
+            user.token = jwt.CreateToken(user.id_user);
+
+            return user;
+        } else {
+            return [];
+        }
+    }
+};
+
+export default { Inserir, Login, Profile, InserirAdmin, LoginAdmin };
